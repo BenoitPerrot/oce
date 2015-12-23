9fe5ce7fafbb56bf415c5031f9a425afb99e91f0
BenoitPerrot <benoit.noe.perrot@gmail.com>
2015-12-23 15:52:48 +0100

    inc: Move DPrsStd headers to Viewer/DPrsStd

--- src/Viewer/DPrsStd/DPrsStd.cxx
+++ src/Viewer/DPrsStd/DPrsStd.cxx
@@ -14,7 +14,7 @@
 // Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
-#include <DPrsStd.hxx>
+#include <Viewer/DPrsStd/DPrsStd.hxx>
 #include <Draw_Interpretor.hxx>
 #include <Draw.hxx>
 
